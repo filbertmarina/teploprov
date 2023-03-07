105dd652d094b736286a605b3c4b8ebab39e8ae5
filbertmarina <112921911+filbertmarina@users.noreply.github.com>
2023-03-08 03:45:56 +0700

    Update teplo.c

--- teplo.c
+++ teplo.c
@@ -4,57 +4,62 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-int SIZE; //size of matrix
-int iter_max;
-double err_max;
-int main(int argc, char* argv[]) {
+int main(double argc, int argv, int argw) {
 	clock_t start;
 	start = clock();
 
-	scanf ("%lf %d %d", &err_max, &SIZE, &iter_max);
-	double** mas = (double**)malloc(SIZE * sizeof(double*));
-	for (int i = 0; i < SIZE; i++)
-		mas[i] = (double*)calloc(SIZE, sizeof(double));
+	double** mas = (double**)malloc(argv * sizeof(double*));
+	for (int i = 0; i < argv; i++)
+		mas[i] = (double*)calloc(argv, sizeof(double));
 	
 
-	double** mas_old = (double**)malloc(SIZE * sizeof(double*));
-	for (int i = 0; i < SIZE; i++)
-		mas_old[i] = (double*)calloc(SIZE, sizeof(double));
+	double** mas_old = (double**)malloc(argv * sizeof(double*));
+	for (int i = 0; i < argv; i++)
+		mas_old[i] = (double*)calloc(argv, sizeof(double));
 
 	mas[0][0] = 10;
-	mas[SIZE-1][0] = 20;//1 -ńňđîęč 2- ńňëá
-	mas[0][SIZE-1] = 20;
-	mas[SIZE-1][SIZE-1] = 30;
+	mas[argv-1][0] = 20;//1 -строки 2- стлб
+	mas[0][argv-1] = 20;
+	mas[argv-1][argv-1] = 30;
 
-	for (int i = 1; i < SIZE-1; i++) {
-		mas[0][i] = mas[0][i-1]+ (mas[0][SIZE-1] - mas[0][0] )/ SIZE;
-		mas[i][0] = mas[i-1][0]+   (mas[SIZE-1][0] - mas[0][0]) / SIZE;
-		mas[SIZE-1][i] = mas[SIZE - 1][i-1]+  (mas[SIZE-1][SIZE-1] - mas[SIZE-1][0]) / SIZE;
-		mas[i][SIZE-1] = mas[i-1][SIZE - 1] + (mas[SIZE-1][SIZE-1] - mas[0][SIZE-1]) / SIZE;
+	for (int i = 1; i < argv-1; i++) {
+		mas[0][i] = mas[0][i-1]+ (mas[0][argv-1] - mas[0][0] )/ argv;
+		mas[i][0] = mas[i-1][0]+   (mas[argv-1][0] - mas[0][0]) / argv;
+		mas[argv-1][i] = mas[argv - 1][i-1]+  (mas[argv-1][argv-1] - mas[argv-1][0]) / argv;
+		mas[i][argv-1] = mas[i-1][argv - 1] + (mas[argv-1][argv-1] - mas[0][argv-1]) / argv;
 	}
 
 
 	int iter = 0;
-	double err = err_max*10;  //10^(-6)
+	double err = argc;  //10^(-6)
 
-	for (int i = 0; i < SIZE; i++) 
-		for (int j = 0; j < SIZE; j++)
+	for (int i = 0; i < argv; i++) 
+		for (int j = 0; j < argv; j++)
 			mas_old[i][j] = mas[i][j];
 
-#pragma acc data copyin(err, SIZE, mas[0:SIZE][0:SIZE], mas_old[0:SIZE][0:SIZE])
-	while ((err > err_max || iter ==1) && iter < iter_max) {
+#pragma acc data copyin(err, argv, mas[0:argv][0:argv], mas_old[0:argv][0:argv])
+	while ((err > argc || iter ==1) && iter < argw) {
+		if (iter%100==0){
+			err = 0;
+#pragma acc update device(err)
+		}
 		iter += 1;
 		err = 0;
-#pragma acc parallel loop async 
-		for (int i = 1; i < SIZE - 1; i++) 
-			for (int j = 1; j < SIZE - 1; j++) {
+
+#pragma acc data present(mas_old, mas, err)
+#pragma acc parallel loop independent collapse(2)
+		for (int i = 1; i < argv - 1; i++) {
+			for (int j = 1; j < argv - 1; j++) {
 				mas[i][j] = (mas_old[i - 1][j] + mas_old[i][j - 1] + mas_old[i + 1][j] + mas_old[i][j + 1]) / 4;
 				err = fmax(err, fabs(mas[i][j] - mas_old[i][j]));
 
+
 			}
-#pragma acc update self(mas[1:SIZE-1][1:SIZE-1]) async
-#pragma acc wait
+			if (iter % 100 == 0) {
+#pragma acc update host(err)
+			}
+		}
+
 		
 
 		double **m= mas;
@@ -69,10 +74,10 @@ int main(int argc, char* argv[]) {
 	}
 	double t = (double)(clock() - start) / CLOCKS_PER_SEC;
 	printf(" time: %lf\n", t);
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < argv; i++)
 		free(mas_old[i]);
 	free(mas_old);
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < argv; i++)
 		free(mas[i]);
 	free(mas);
 
